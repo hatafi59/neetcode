e7e7474a024bf7b22088c03821535153f8924d91
abdelaziz hatafi <167553698+hatafi59@users.noreply.github.com>
2026-08-19 11:58:29 +0100

    Add: climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/climbing-stairs/submission-3.cpp
@@ -0,0 +1,23 @@
+class Solution {
+    vector<int> dp;
+    
+    int verify(int n,vector<int> &dp ){
+        if(n<0) return 0;
+        if(n==1 || n==0) return 1;
+        if(dp[n] != -1) return dp[n];
+        return dp[n] = verify(n-1,dp)+verify(n-2,dp) ;
+
+    }
+public:
+    int climbStairs(int n) {
+        dp.assign(n+1,-1);
+        
+        dp[0]=1; dp[1]=1;
+        for(int i=2 ; i<n+1;i++)
+            dp[i]=dp[i-1]+dp[i-2];
+
+        return dp[n];
+        //return verify(n,dp);
+
+    }
+};
